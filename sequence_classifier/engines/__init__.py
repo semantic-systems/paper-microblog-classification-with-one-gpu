708dc3cc7a1cb20394381ae578bf0cccaaf06674
Junbo Huang <junbo.huang@uni-hamburg.de>
2024-09-30 16:56:10 +0200

    :sparkles: new experiments for reviews

--- sequence_classifier/engines/__init__.py
+++ sequence_classifier/engines/__init__.py
@@ -1,3 +1,3 @@
 from sequence_classifier.engines.agent import Agent, BatchLearningAgent
 from sequence_classifier.engines.environment import Environment, StaticEnvironment, DynamicEnvironment
-from sequence_classifier.engines.trainer import Trainer, MetaLearningTrainer, BatchLearningTrainer, MultiAgentTrainer, SingleAgentTrainer
+from sequence_classifier.engines.trainer import Trainer, BatchLearningTrainer, MultiAgentTrainer, SingleAgentTrainer
